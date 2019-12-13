8c285b4cc849d2ee2ceeac3ec9bbece1425dc43e
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2019-12-13 17:11:26 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: 'Kim1',
+    birthday: '971222',
+    gender: 'Man',
+    job: 'Programmer'
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: 'Kim2',
+    birthday: '971222',
+    gender: 'Man',
+    job: 'Programmer'
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: 'Kim3',
+    birthday: '971222',
+    gender: 'Man',
+    job: 'Programmer'
+  },
+  {
+    id: 4,
+    image: 'https://placeimg.com/64/64/4',
+    name: 'Kim4',
+    birthday: '971222',
+    gender: 'Man',
+    job: 'Programmer'
+  },
+  {
+    id: 5,
+    image: 'https://placeimg.com/64/64/5',
+    name: 'Kim5',
+    birthday: '971222',
+    gender: 'Man',
+    job: 'Programmer'
+  }
+]
+
+class App extends Component {
+ 
+  render() {
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer 
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
